1aa716e3ac670da8a123b7818ebe5bc834a7124d
Justin Wyer <justin@lifeasageek.com>
2014-11-23 18:52:18 +0200

    return 204 for bad auth

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.get('/auth', function (req, res) {
     if (err) {
       res.status(500).end();
     } else if (user == null) {
-      res.status(403).end();
+      res.status(204).end();
     } else {
       bcrypt.compare(req.param('password'), user.password, function(err, res) {
         if (err) {
-          res.status(403).end();
+          res.status(204).end();
         } else {
           res.status(200).end();
         }
