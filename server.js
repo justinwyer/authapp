659be344cdea53a318dac1d3066b3d55ff950b9a
Justin Wyer <justin@lifeasageek.com>
2014-11-23 17:13:25 +0200

    initial app

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var Promise = require('bluebird');
+var mongodb = require("mongodb");
+var bcrypt = require("bcrypt");
+
+var app = express();
+var db;
+
+app.get('/ping', function (req, res) {
+  res.send('pong')
+});
+
+app.get('/auth', function (req, res) {
+  db.collection.findOne({username: req.param('username')}, function(err, user) {
+    bcrypt.compare(req.param('password'), user.password, function(err, res) {
+      if (err) {
+        res.status(403).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  }); 
+});
+
+var server = app.listen(80, function () {
+  db = MongoClient.connect('mongodb://172.31.20.72:27017/authentication', function(err, db) {
+    console.log("Connected correctly to server");
+  });
+});
