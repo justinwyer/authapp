3ab68ba80627612a8935324f08ee3f9df5dd9e72
Justin Wyer <justin@lifeasageek.com>
2014-11-25 12:19:09 +0200

    use postgres

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var Promise = require('bluebird');
-var mongodb = require("mongodb");
 var bcrypt = require("bcryptjs");
-var MongoClient = require('mongodb').MongoClient
+
+var pg = require('pg');
+var conString = "postgres://auth:password@auth.ck2fdib66dje.eu-west-1.rds.amazonaws.com:5432/authusers";
 
 var app = express();
 var db;
@@ -16,26 +17,29 @@ app.get('/loaderio-ec93d5d4a283b4c13d2a854bbc7b2806', function (req, res) {
 });
 
 app.get('/auth', function (req, res) {
-  db.collection('users').findOne({username: req.param('username')}, function(err, user) {
-    if (err) {
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
       res.sendStatus(500);
-    } else if (user == null) {
-      res.sendStatus(204);
-    } else {
-      bcrypt.compare(req.param('password'), user.hashedPassword, function(err, result) {
-        if (err || !result) {
-          res.sendStatus(204);
-        } else {
-          res.sendStatus(200);
-        }
-      });  
     }
-  }); 
+    client.query('SELECT * FROM users WHERE username = $1', [req.param('username')], function(err, result) {
+      done();
+      if(err) {
+        res.sendStatus(500);
+      }
+      if (result.rows.length > 0) {
+        bcrypt.compare(req.param('password'), result.rows[0].hashedPassword, function(err, result) {
+          if (err || !result) {
+            res.sendStatus(204);
+          } else {
+            res.sendStatus(200);
+          }
+        });
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  });
 });
 
 var server = app.listen(80, function () {
-  MongoClient.connect('mongodb://172.31.23.0:27017/authentication', function(err, _db) {
-    console.log("Connected correctly to server");
-    db = _db;
-  });
 });
